c3152de8ea6a8cc51f1116c269bca35aaedb4b63
Taniya Das <tdas@codeaurora.org>
2018-12-14 12:36:24 +0530

    clk: qcom: rcg2: Add support for hardware control mode
    Add a flag to indicate to support and enable hardware control mode
    of an RCG.
    
    Change-Id: I21194a463441ea13b7575a4174b1f96504b42b1e
    Signed-off-by: Taniya Das <tdas@codeaurora.org>

--- drivers/clk/qcom/clk-rcg2.c
+++ drivers/clk/qcom/clk-rcg2.c
@@ -440,6 +440,8 @@ static int __clk_rcg2_configure(struct clk_rcg2 *rcg, const struct freq_tbl *f,
 	cfg |= rcg->parent_map[index].cfg << CFG_SRC_SEL_SHIFT;
 	if (rcg->mnd_width && f->n && (f->m != f->n))
 		cfg |= CFG_MODE_DUAL_EDGE;
+	if (rcg->flags & HW_CLK_CTRL_MODE)
+		cfg |= CFG_HW_CLK_CTRL_MASK;
 
 	*_cfg &= ~mask;
 	*_cfg |= cfg;
