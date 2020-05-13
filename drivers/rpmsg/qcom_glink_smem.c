541274d6d8773af646f49bda873269d587f9830f
Arun Prakash <app@codeaurora.org>
2020-05-13 10:30:22 +0530

    rpmsg: glink: Append glink irq name with remote pid
    Glink irq for all the remote edges are registered with same name
    glink-smem. It is difficult to distinguish each irq in logs.
    
    Appending remote pid to generic irq name.
    
    Change-Id: I6ead87e0e4adae204535b599d07d9471387c861b
    Signed-off-by: Deepak Kumar Singh <deesin@codeaurora.org>
    Signed-off-by: Arun Prakash <app@codeaurora.org>

--- drivers/rpmsg/qcom_glink_smem.c
+++ drivers/rpmsg/qcom_glink_smem.c
@@ -39,6 +39,7 @@ struct qcom_glink_smem {
 	struct device dev;
 
 	int irq;
+	char irqname[32];
 	struct qcom_glink *glink;
 
 	struct mbox_client mbox_client;
@@ -304,10 +305,11 @@ struct qcom_glink_smem *qcom_glink_smem_register(struct device *parent,
 		goto err_put_dev;
 	}
 
+	scnprintf(smem->irqname, 32, "glink-native-%u", remote_pid);
 	smem->irq = of_irq_get(smem->dev.of_node, 0);
 	ret = devm_request_irq(&smem->dev, smem->irq, qcom_glink_smem_intr,
 			       IRQF_NO_SUSPEND | IRQF_NO_AUTOEN,
-			       "glink-smem", smem);
+			       smem->irqname, smem);
 	if (ret) {
 		dev_err(&smem->dev, "failed to request IRQ\n");
 		goto err_put_dev;
