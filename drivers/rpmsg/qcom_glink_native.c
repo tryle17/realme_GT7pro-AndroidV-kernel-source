de66c37d8465cd20b9122990216d9a4b8f85e4cb
Chris Lew <quic_clew@quicinc.com>
2022-05-16 17:37:49 -0700

    rpmsg: glink: rename __qcom_glink_rx_done
    Rename the internal __qcom_glink_rx_done that forms the packets and
    sends the command on the wire. This will help with patch organization
    related to the upcoming rpmsg_rx_done changes.
    
    Change-Id: If1c816f9dc29e213997fbdda6c4f56348a94a13e
    Signed-off-by: Chris Lew <quic_clew@quicinc.com>

--- drivers/rpmsg/qcom_glink_native.c
+++ drivers/rpmsg/qcom_glink_native.c
@@ -525,7 +525,7 @@ static void qcom_glink_send_close_ack(struct qcom_glink *glink,
 	qcom_glink_tx(glink, &req, sizeof(req), NULL, 0, true);
 }
 
-static int __qcom_glink_rx_done(struct qcom_glink *glink,
+static int qcom_glink_send_rx_done(struct qcom_glink *glink,
 				struct glink_channel *channel,
 				struct glink_core_rx_intent *intent,
 				bool wait)
@@ -569,7 +569,7 @@ static void qcom_glink_rx_done_work(struct kthread_work *work)
 		list_del(&intent->node);
 		spin_unlock_irqrestore(&channel->intent_lock, flags);
 
-		__qcom_glink_rx_done(glink, channel, intent, true);
+		qcom_glink_send_rx_done(glink, channel, intent, true);
 
 		spin_lock_irqsave(&channel->intent_lock, flags);
 	}
@@ -599,7 +599,7 @@ static void qcom_glink_rx_done(struct qcom_glink *glink,
 	/* Schedule the sending of a rx_done indication */
 	spin_lock(&channel->intent_lock);
 	if (list_empty(&channel->done_intents))
-		ret = __qcom_glink_rx_done(glink, channel, intent, false);
+		ret = qcom_glink_send_rx_done(glink, channel, intent, false);
 
 	if (ret) {
 		list_add_tail(&intent->node, &channel->done_intents);
