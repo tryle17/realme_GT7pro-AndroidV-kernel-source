72fa3fd6311d8e5133bd48a3bae529b18092f45d
Mukesh Ojha <quic_mojha@quicinc.com>
2022-11-18 10:28:34 +0530

    remoteproc: qcom: pas: Fix subdevice add order
    The subdevice list should be populated in a particular order as
    the state notifications have a priority. This change re-orders
    the subdevice add calls to ensure higher priority subdevices are
    present at the start of the list.
    
    Change-Id: I9f98c95a77bcdd783e36b959836a19ff06da9526
    Signed-off-by: Mukesh Ojha <quic_mojha@quicinc.com>
    Signed-off-by: Naman Jain <quic_namajain@quicinc.com>
    Signed-off-by: Gokul krishna Krishnakumar <quic_gokukris@quicinc.com>

--- drivers/remoteproc/qcom_q6v5_pas.c
+++ drivers/remoteproc/qcom_q6v5_pas.c
@@ -618,7 +618,6 @@ static int adsp_probe(struct platform_device *pdev)
 
 	qcom_add_glink_subdev(rproc, &adsp->glink_subdev, desc->ssr_name);
 	qcom_add_smd_subdev(rproc, &adsp->smd_subdev);
-	qcom_add_ssr_subdev(rproc, &adsp->ssr_subdev, desc->ssr_name);
 	adsp->sysmon = qcom_add_sysmon_subdev(rproc,
 					      desc->sysmon_name,
 					      desc->ssctl_id);
@@ -627,6 +626,7 @@ static int adsp_probe(struct platform_device *pdev)
 		goto detach_proxy_pds;
 	}
 
+	qcom_add_ssr_subdev(rproc, &adsp->ssr_subdev, desc->ssr_name);
 	ret = device_create_file(adsp->dev, &dev_attr_txn_id);
 	if (ret)
 		goto remove_subdevs;
