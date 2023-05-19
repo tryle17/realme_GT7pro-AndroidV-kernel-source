2e7f9f4328b6766e0ff70cb9a57b1cae6100a295
Patrick Daly <quic_pdaly@quicinc.com>
2023-05-19 12:04:08 -0700

    Revert "dma-heap: qcom: Add deferred_free functionality to system-heap"
    This partially reverts commit d128b1534dc0 ("dma-heap: qcom: Add
    deferred_free functionality to system-heap").
    
    Resolve merge conflicts in qcom_system_heap_free due to addition of
    system-movable heap.
    
    Additionally, do not add a helper function for kfree'ing
    struct qcom_sg_buffer. This reduces code churn in areas which are
    not strictly related to deferred-free feature.
    
    Change-Id: Ib659d511915c9e0d08bc189d608f8480ff10d7fb
    Signed-off-by: Patrick Daly <quic_pdaly@quicinc.com>

--- drivers/dma-buf/heaps/qcom_system_heap.c
+++ drivers/dma-buf/heaps/qcom_system_heap.c
@@ -341,53 +341,39 @@ static int system_heap_zero_buffer(struct qcom_sg_buffer *buffer)
 	return ret;
 }
 
-static void system_heap_buf_free(struct deferred_freelist_item *item,
-				 enum df_reason reason)
+void qcom_system_heap_free(struct qcom_sg_buffer *buffer)
 {
 	struct qcom_system_heap *sys_heap;
-	struct qcom_sg_buffer *buffer;
 	struct sg_table *table;
 	struct scatterlist *sg;
 	int i, j;
 
-	buffer = container_of(item, struct qcom_sg_buffer, deferred_free);
 	sys_heap = dma_heap_get_drvdata(buffer->heap);
-	/* Zero the buffer pages before adding back to the pool */
-	if (reason == DF_NORMAL)
-		if (system_heap_zero_buffer(buffer))
-			reason = DF_UNDER_PRESSURE; // On failure, just free
-
 	table = &buffer->sg_table;
+
+	/* Zero the buffer pages before adding back to the pool */
+	system_heap_zero_buffer(buffer);
+
 	for_each_sg(table->sgl, sg, table->nents, i) {
 		struct page *page = sg_page(sg);
 
-		if (reason == DF_UNDER_PRESSURE) {
+		for (j = 0; j < NUM_ORDERS; j++) {
+			if (compound_order(page) == orders[j])
+				break;
+		}
+
+		/* Do not keep page in the pool if it is a zone movable page */
+		if (is_zone_movable_page(page)) {
+			/* Unpin the page before freeing page back to buddy */
+			put_page(page);
 			__free_pages(page, compound_order(page));
 		} else {
-			for (j = 0; j < NUM_ORDERS; j++) {
-				if (compound_order(page) == orders[j])
-					break;
-			}
-			/* Do not keep page in the pool if it is a zone movable page */
-			if (is_zone_movable_page(page)) {
-				/* Unpin the page before freeing page back to buddy */
-				put_page(page);
-				__free_pages(page, compound_order(page));
-			} else {
-				dynamic_page_pool_free(sys_heap->pool_list[j], page);
-			}
+			dynamic_page_pool_free(sys_heap->pool_list[j], page);
 		}
 	}
 	sg_free_table(table);
 	kfree(buffer);
 }
-
-void qcom_system_heap_free(struct qcom_sg_buffer *buffer)
-{
-	deferred_free(&buffer->deferred_free, system_heap_buf_free,
-			PAGE_ALIGN(buffer->len) / PAGE_SIZE);
-}
-
 
 struct page *qcom_sys_heap_alloc_largest_available(struct dynamic_page_pool **pools,
 						   unsigned long size,
