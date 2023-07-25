e3a3fb20c44d05f3b1e7d6d19c4b258797c414e5
Stephen Dickey <quic_dickey@quicinc.com>
2023-07-25 12:14:21 -0700

    sched/walt: pipeline_set_boost must handle auto and manual pipeline
    pipeline_set_boost does not handle the case properly, where both
    manual and auto pipeline hinting are enabled or disabled, independently.
    For example, this series of events
    
     pipeline_set_boost(true, MANUAL_PIPELINE);
      - auto pipeline enabled
     pipeline_set_boost(true, AUTO_PIPELINE);
      - auto pipeline disabled\
     pipeline_set_boost(false, MANUAL_PIPELINE);
    
    With the above, pipeline boost will be enabled, even though manual
    pipeline is no longer requesting it be enabled, and auto pipeline
    is disabled.
    
    Correct the code to independently track state of the AUTO or MANUAL
    pipeline, and using that information to decide if boost is currently
    requested or not.
    
    Change-Id: Ia31eb8cd45b417f55f1e6827953073f708820ce6
    Signed-off-by: Stephen Dickey <quic_dickey@quicinc.com>

--- kernel/sched/walt/walt.c
+++ kernel/sched/walt/walt.c
@@ -84,7 +84,7 @@ unsigned int __read_mostly sched_init_task_load_windows;
  */
 unsigned int __read_mostly sched_load_granule;
 
-int enable_shared_rail_boost;
+unsigned int enable_pipeline_boost;
 
 u64 walt_sched_clock(void)
 {
@@ -698,7 +698,7 @@ cpu_util_freq_walt(int cpu, struct walt_cpu_load *walt_load, unsigned int *reaso
 	util =  __cpu_util_freq_walt(cpu, walt_load, reason);
 
 	if (cpumask_test_cpu(cpu, &shared_rail_sibling_cpus) &&
-			enable_shared_rail_boost) {
+			enable_pipeline_boost) {
 		for_each_cpu(i, &shared_rail_sibling_cpus) {
 			if (i == (num_possible_cpus() - 1))
 				util_prime = __cpu_util_freq_walt(i, &wl_prime, reason);
@@ -3728,7 +3728,12 @@ static inline void pipeline_set_boost(bool boost, int flag)
 	static bool isolation_boost;
 	struct walt_sched_cluster *cluster;
 
-	if (isolation_boost && !boost) {
+	if (!boost)
+		enable_pipeline_boost &= ~flag;
+	else
+		enable_pipeline_boost |= flag;
+
+	if (isolation_boost && !enable_pipeline_boost) {
 		isolation_boost = false;
 
 		for_each_sched_cluster(cluster) {
@@ -3736,8 +3741,7 @@ static inline void pipeline_set_boost(bool boost, int flag)
 			    is_max_possible_cluster_cpu(cpumask_first(&cluster->cpus)))
 				core_ctl_set_cluster_boost(cluster->id, false);
 		}
-		enable_shared_rail_boost &= ~flag;
-	} else if (!isolation_boost && boost) {
+	} else if (!isolation_boost && enable_pipeline_boost) {
 		isolation_boost = true;
 
 		for_each_sched_cluster(cluster) {
@@ -3745,7 +3749,26 @@ static inline void pipeline_set_boost(bool boost, int flag)
 			    is_max_possible_cluster_cpu(cpumask_first(&cluster->cpus)))
 				core_ctl_set_cluster_boost(cluster->id, true);
 		}
-		enable_shared_rail_boost |= flag;
+	}
+}
+
+/*
+ * sysctl_sched_heavy_nr can change at any moment in time. as a result,
+ * the ability to set/clear boost state for a particular type of pipeline,
+ * is hindered. Detect a transition and reset the boost state of the pipeline
+ * method no longer in use.
+ */
+static inline void pipeline_reset_boost(void)
+{
+	static unsigned int last_sched_heavy_nr;
+
+	if (sysctl_sched_heavy_nr != last_sched_heavy_nr) {
+		if (sysctl_sched_heavy_nr)
+			pipeline_set_boost(MANUAL_PIPELINE, false);
+		else
+			pipeline_set_boost(AUTO_PIPELINE, false);
+
+		last_sched_heavy_nr = sysctl_sched_heavy_nr;
 	}
 }
 
@@ -4312,7 +4335,7 @@ static void walt_irq_work(struct irq_work *irq_work)
 			return;
 
 		if (cpumask_intersects(&lock_cpus, &shared_rail_sibling_cpus) &&
-				enable_shared_rail_boost) {
+				enable_pipeline_boost) {
 			cpumask_or(&lock_cpus, &lock_cpus, &shared_rail_sibling_cpus);
 			is_shared_rail_migration = true;
 		}
@@ -4349,6 +4372,7 @@ static void walt_irq_work(struct irq_work *irq_work)
 		find_heaviest_topapp(wrq->window_start);
 		rearrange_heavy(wrq->window_start);
 		rearrange_pipeline_preferred_cpus(wrq->window_start);
+		pipeline_reset_boost();
 		core_ctl_check(wrq->window_start, wakeup_ctr_sum);
 	}
 }
