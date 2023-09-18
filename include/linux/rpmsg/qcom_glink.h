831c652fd694c8d6811784cb0838598c7e79ae9e
Chris Lew <quic_clew@quicinc.com>
2023-09-18 13:07:06 -0700

    rpmsg: glink: spss: Add snapshot of spss transport
    This snapshot is taken as of qcom-6.1 'commit <04410dc71222>
    ("msm_geni_serial: Prevent unclocked access in msm_geni_serial_flush")'.
    
    Move the irq handling into the transport and adjust the apis to accept
    and return struct qcom_glink_spss *.
    
    Change-Id: Ifb19f2867c40d91b108d90437be0b4ccd936664c
    Signed-off-by: Chris Lew <quic_clew@quicinc.com>

--- include/linux/rpmsg/qcom_glink.h
+++ include/linux/rpmsg/qcom_glink.h
@@ -1,4 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
 
 #ifndef _LINUX_RPMSG_QCOM_GLINK_H
 #define _LINUX_RPMSG_QCOM_GLINK_H
@@ -6,6 +9,7 @@
 #include <linux/device.h>
 
 struct qcom_glink_smem;
+struct qcom_glink_spss;
 
 #if IS_ENABLED(CONFIG_RPMSG_QCOM_GLINK)
 void qcom_glink_ssr_notify(const char *ssr_name);
@@ -43,6 +47,25 @@ static inline bool qcom_glink_is_wakeup(bool reset)
 {
 	return false;
 }
+#endif
+
+#if IS_ENABLED(CONFIG_RPMSG_QCOM_GLINK_SPSS)
+
+struct qcom_glink_spss *qcom_glink_spss_register(struct device *parent,
+						 struct device_node *node);
+void qcom_glink_spss_unregister(struct qcom_glink_spss *glink);
+
+#else
+
+static inline struct qcom_glink_spss *
+qcom_glink_spss_register(struct device *parent,
+			 struct device_node *node)
+{
+	return NULL;
+}
+
+static inline void qcom_glink_spss_unregister(struct qcom_glink_spss *glink) {}
+
 #endif
 
 #endif
