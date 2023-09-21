torvalds/linux

mem-buf: Add mem_buf_dma_buf_get_vmperm()
Add mem_buf_dma_buf_get_vmperm() to allow people to get read-only
access to the VMIDs and permissions of a DMA-BUF.

Change-Id: Ie48ecd7db1cfa7c4339f9c1194d26a6bfef31a11
Signed-off-by: Chris Goldsworthy <quic_cgoldswo@quicinc.com>

drivers/soc/qcom/mem_buf/mem_buf_dma_buf.c
@@ -640,6 +640,26 @@ bool mem_buf_dma_buf_exclusive_owner(struct dma_buf *dmabuf)
 }
 EXPORT_SYMBOL(mem_buf_dma_buf_exclusive_owner);
 
+int mem_buf_dma_buf_get_vmperm(struct dma_buf *dmabuf, const int **vmids,
+		const int **perms, int *nr_acl_entries)
+{
+	struct mem_buf_vmperm *vmperm;
+
+	vmperm = to_mem_buf_vmperm(dmabuf);
+	if (IS_ERR(vmperm))
+		return PTR_ERR(vmperm);
+
+	mutex_lock(&vmperm->lock);
+
+	*vmids = vmperm->vmids;
+	*perms = vmperm->perms;
+	*nr_acl_entries = vmperm->nr_acl_entries;
+
+	mutex_unlock(&vmperm->lock);
+	return 0;
+}
+EXPORT_SYMBOL(mem_buf_dma_buf_get_vmperm);
+
 int mem_buf_dma_buf_copy_vmperm(struct dma_buf *dmabuf, int **vmids,
 		int **perms, int *nr_acl_entries)
 {

tryle17/realme_GT7pro-AndroidV-kernel-source

drivers/soc/qcom/mem_buf/mem_buf_dma_buf.c
@@ -640,6 +640,26 @@ bool mem_buf_dma_buf_exclusive_owner(struct dma_buf *dmabuf)
 }
 EXPORT_SYMBOL(mem_buf_dma_buf_exclusive_owner);
 
+int mem_buf_dma_buf_get_vmperm(struct dma_buf *dmabuf, const int **vmids,
+		const int **perms, int *nr_acl_entries)
+{
+	struct mem_buf_vmperm *vmperm;
+
+	vmperm = to_mem_buf_vmperm(dmabuf);
+	if (IS_ERR(vmperm))
+		return PTR_ERR(vmperm);
+
+	mutex_lock(&vmperm->lock);
+
+	*vmids = vmperm->vmids;
+	*perms = vmperm->perms;
+	*nr_acl_entries = vmperm->nr_acl_entries;
+
+	mutex_unlock(&vmperm->lock);
+	return 0;
+}
+EXPORT_SYMBOL_GPL(mem_buf_dma_buf_get_vmperm);
+
 int mem_buf_dma_buf_copy_vmperm(struct dma_buf *dmabuf, int **vmids,
 		int **perms, int *nr_acl_entries)
 {