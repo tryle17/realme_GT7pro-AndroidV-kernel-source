MiCode/Xiaomi_Kernel_OpenSource

thermal: qcom-spmi-adc-tm5: Add support to register ADC channels without trips
Sometimes it is required to expose ADC channels to thermal framework
when there are no ADC_TM channels available.

Add support to include channels under ADC_TM to register with thermal
framework, for temperature reading alone, without registering interrupts.

This will help to support more ADC channels when there is HW limitation
for number of channels which can be supported with ADC_TM interrupt
functionality.

Change-Id: Iac335fa75a92097199106aa88a4ae05bc15e2f33
Signed-off-by: Archana Sriram <quic_c_apsrir@quicinc.com>

drivers/thermal/qcom/qcom-spmi-adc-tm5.c
@@ -4,6 +4,7 @@
  *
  * Based on original driver:
  * Copyright (c) 2012-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 #include <linux/bitfield.h>
 #include <linux/iio/adc/qcom-vadc-common.h>
@@ -346,7 +347,11 @@ static struct thermal_zone_of_device_ops adc_tm5_ops = {
 	.set_trips = adc_tm5_set_trips,
 };
 
-static int adc_tm5_register_tzd(struct adc_tm5_chip *adc_tm)
+static struct thermal_zone_of_device_ops adc_tm5_iio_ops = {
+	.get_temp = adc_tm5_get_temp,
+};
+
+static int adc_tm5_register_tzd(struct adc_tm5_chip *adc_tm, bool set_trips)
 {
 	unsigned int i;
 	struct thermal_zone_device *tzd;
@@ -354,10 +359,17 @@ static int adc_tm5_register_tzd(struct adc_tm5_chip *adc_tm)
 	for (i = 0; i < adc_tm->nchannels; i++) {
 		adc_tm->channels[i].chip = adc_tm;
 
-		tzd = devm_thermal_zone_of_sensor_register(adc_tm->dev,
-							   adc_tm->channels[i].channel,
-							   &adc_tm->channels[i],
-							   &adc_tm5_ops);
+		if (set_trips)
+			tzd = devm_thermal_zone_of_sensor_register(adc_tm->dev,
+						   adc_tm->channels[i].channel,
+						   &adc_tm->channels[i],
+						   &adc_tm5_ops);
+		else
+			tzd = devm_thermal_zone_of_sensor_register(adc_tm->dev,
+						   adc_tm->channels[i].channel,
+						   &adc_tm->channels[i],
+						   &adc_tm5_iio_ops);
+
 		if (IS_ERR(tzd)) {
 			if (PTR_ERR(tzd) == -ENODEV) {
 				dev_warn(adc_tm->dev, "thermal sensor on channel %d is not used\n",
@@ -579,16 +591,24 @@ static int adc_tm5_probe(struct platform_device *pdev)
 	adc_tm->dev = dev;
 	adc_tm->base = reg;
 
-	irq = platform_get_irq(pdev, 0);
-	if (irq < 0) {
-		dev_err(dev, "get_irq failed: %d\n", irq);
-		return irq;
-	}
-
 	ret = adc_tm5_get_dt_data(adc_tm, node);
 	if (ret) {
 		dev_err(dev, "get dt data failed: %d\n", ret);
 		return ret;
+	}
+
+	if (of_device_is_compatible(node, "qcom,spmi-adc-tm5-iio")) {
+		ret = adc_tm5_register_tzd(adc_tm, false);
+		if (ret)
+			dev_err(dev, "tzd register failed for adc tm5 iio channel\n");
+
+		return ret;
+	}
+
+	irq = platform_get_irq(pdev, 0);
+	if (irq < 0) {
+		dev_err(dev, "get_irq failed: %d\n", irq);
+		return irq;
 	}
 
 	ret = adc_tm5_init(adc_tm);
@@ -597,7 +617,7 @@ static int adc_tm5_probe(struct platform_device *pdev)
 		return ret;
 	}
 
-	ret = adc_tm5_register_tzd(adc_tm);
+	ret = adc_tm5_register_tzd(adc_tm, true);
 	if (ret) {
 		dev_err(dev, "tzd register failed\n");
 		return ret;
@@ -610,6 +630,10 @@ static int adc_tm5_probe(struct platform_device *pdev)
 static const struct of_device_id adc_tm5_match_table[] = {
 	{
 		.compatible = "qcom,spmi-adc-tm5",
+		.data = &adc_tm5_data_pmic,
+	},
+	{
+		.compatible = "qcom,spmi-adc-tm5-iio",
 		.data = &adc_tm5_data_pmic,
 	},
 	{ }

tryle17/realme_GT7pro-AndroidV-kernel-source

drivers/thermal/qcom/qcom-spmi-adc-tm5.c
@@ -666,17 +666,28 @@ static const struct thermal_zone_device_ops adc_tm5_thermal_ops = {
 	.set_trips = adc_tm5_set_trips,
 };
 
-static int adc_tm5_register_tzd(struct adc_tm5_chip *adc_tm)
+static const struct thermal_zone_device_ops adc_tm5_thermal_iio_ops = {
+	.get_temp = adc_tm5_get_temp,
+};
+
+static int adc_tm5_register_tzd(struct adc_tm5_chip *adc_tm, bool set_trips)
 {
 	unsigned int i;
 	struct thermal_zone_device *tzd;
 
 	for (i = 0; i < adc_tm->nchannels; i++) {
 		adc_tm->channels[i].chip = adc_tm;
-		tzd = devm_thermal_of_zone_register(adc_tm->dev,
-						    adc_tm->channels[i].channel,
-						    &adc_tm->channels[i],
-						    &adc_tm5_thermal_ops);
+		if (set_trips)
+			tzd = devm_thermal_of_zone_register(adc_tm->dev,
+						   adc_tm->channels[i].channel,
+						   &adc_tm->channels[i],
+						   &adc_tm5_thermal_ops);
+		else
+			tzd = devm_thermal_of_zone_register(adc_tm->dev,
+						   adc_tm->channels[i].channel,
+						   &adc_tm->channels[i],
+						   &adc_tm5_thermal_iio_ops);
+
 		if (IS_ERR(tzd)) {
 			if (PTR_ERR(tzd) == -ENODEV) {
 				dev_dbg(adc_tm->dev, "thermal sensor on channel %d is not used\n",
@@ -1026,13 +1037,21 @@ static int adc_tm5_probe(struct platform_device *pdev)
 	adc_tm->dev = dev;
 	adc_tm->base = reg;
 
-	irq = platform_get_irq(pdev, 0);
-	if (irq < 0)
-		return irq;
-
 	ret = adc_tm5_get_dt_data(adc_tm, node);
 	if (ret)
 		return dev_err_probe(dev, ret, "get dt data failed\n");
+
+	if (of_device_is_compatible(node, "qcom,spmi-adc-tm5-iio")) {
+		ret = adc_tm5_register_tzd(adc_tm, false);
+		if (ret)
+			dev_err(dev, "tzd register failed for adc tm5 iio channel\n");
+
+		return ret;
+	}
+
+	irq = platform_get_irq(pdev, 0);
+	if (irq < 0)
+		return irq;
 
 	ret = adc_tm->data->init(adc_tm);
 	if (ret) {
@@ -1040,7 +1059,7 @@ static int adc_tm5_probe(struct platform_device *pdev)
 		return ret;
 	}
 
-	ret = adc_tm5_register_tzd(adc_tm);
+	ret = adc_tm5_register_tzd(adc_tm, true);
 	if (ret) {
 		dev_err(dev, "tzd register failed\n");
 		return ret;
@@ -1062,6 +1081,10 @@ static const struct of_device_id adc_tm5_match_table[] = {
 	{
 		.compatible = "qcom,spmi-adc-tm5-gen2",
 		.data = &adc_tm5_gen2_data_pmic,
+	},
+	{
+		.compatible = "qcom,spmi-adc-tm5-iio",
+		.data = &adc_tm5_data_pmic,
 	},
 	{ }
 };