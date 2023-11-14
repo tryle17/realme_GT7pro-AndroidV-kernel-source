29433874be0ee85c7a6c5e3468592b83c58c1ae0
Wesley Cheng <quic_wcheng@quicinc.com>
2023-11-14 15:32:04 -0800

    usb: phy: phy-msm-m31-eusb2: Fix include path for SCM driver
    Update include path to the proper SCM driver for issuing register
    read/write calls.
    
    Change-Id: I81bb27a470ef979e7d4b41fd04202aeb5bc75015
    Signed-off-by: Wesley Cheng <quic_wcheng@quicinc.com>

--- drivers/usb/phy/phy-msm-m31-eusb2.c
+++ drivers/usb/phy/phy-msm-m31-eusb2.c
@@ -15,7 +15,7 @@
 #include <linux/of_device.h>
 #include <linux/platform_device.h>
 #include <linux/power_supply.h>
-#include <linux/qcom_scm.h>
+#include <linux/firmware/qcom/qcom_scm.h>
 #include <linux/regulator/consumer.h>
 #include <linux/reset.h>
 #include <linux/slab.h>
