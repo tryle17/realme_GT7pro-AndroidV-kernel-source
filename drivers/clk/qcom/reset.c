6b48153c1cb49c365b5ad538878cb83cf2415c16
Mike Tipton <quic_mdtipton@quicinc.com>
2023-12-19 16:37:07 -0800

    clk: qcom: reset: Ensure resets trigger before returning
    The reset control signals are sent through demets, which introduce
    delays between writing the control registers and the reset operations
    actually triggering. We aren't waiting for the demets before returning
    to the client, so the resets may not be triggered yet in all cases. The
    client should be able to assume the operation has actually completed
    after calling reset_assert() or reset_deassert(). Clients should only be
    responsible for ensuring the necessary delay between the assertion and
    de-assertion -- not any internal clock controller delays for triggering
    the resets.
    
    Additionally, writes to these registers may be posted, which means the
    bus may issue an early write ACK before the write is fully propagated to
    the control registers. We aren't reading the register again after
    performing the write, so we might return to the client before the write
    has actually completed.
    
    Ensure the resets are actually triggered before returning to the client
    by reading back the register after writing, and adding the necessary
    demet delays.
    
    Change-Id: I77b4632f3250d655e663bbc405736965ef2d6bad
    Signed-off-by: Mike Tipton <quic_mdtipton@quicinc.com>
    Signed-off-by: Vivek Aknurwar <quic_viveka@quicinc.com>

--- drivers/clk/qcom/reset.c
+++ drivers/clk/qcom/reset.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/bitops.h>
@@ -11,14 +12,43 @@
 
 #include "reset.h"
 
+static void qcom_reset_delay(const struct qcom_reset_map *map)
+{
+	/*
+	 * XO div-4 is commonly used for the reset demets, so by default allow
+	 * enough time for 4 demet cycles at 1.2MHz.
+	 */
+	fsleep(map->udelay ?: 4);
+}
+
 static int qcom_reset(struct reset_controller_dev *rcdev, unsigned long id)
 {
 	struct qcom_reset_controller *rst = to_qcom_reset_controller(rcdev);
 
 	rcdev->ops->assert(rcdev, id);
-	fsleep(rst->reset_map[id].udelay ?: 1); /* use 1 us as default */
-
+	qcom_reset_delay(&rst->reset_map[id]);
 	rcdev->ops->deassert(rcdev, id);
+	return 0;
+}
+
+static int
+qcom_update_reset(struct qcom_reset_controller *rst, const struct qcom_reset_map *map,
+		  u32 mask, u32 val)
+{
+	int ret;
+
+	ret = regmap_update_bits(rst->regmap, map->reg, mask, val);
+	if (ret)
+		return ret;
+
+	/* Ensure the write is fully propagated to the register. */
+	ret = regmap_read(rst->regmap, map->reg, &val);
+	if (ret)
+		return ret;
+
+	/* Give demets a chance to propagate the signal. */
+	qcom_reset_delay(map);
+
 	return 0;
 }
 
@@ -33,7 +63,7 @@ qcom_reset_assert(struct reset_controller_dev *rcdev, unsigned long id)
 	map = &rst->reset_map[id];
 	mask = map->bitmask ? map->bitmask : BIT(map->bit);
 
-	return regmap_update_bits(rst->regmap, map->reg, mask, mask);
+	return qcom_update_reset(rst, map, mask, mask);
 }
 
 static int
@@ -47,7 +77,7 @@ qcom_reset_deassert(struct reset_controller_dev *rcdev, unsigned long id)
 	map = &rst->reset_map[id];
 	mask = map->bitmask ? map->bitmask : BIT(map->bit);
 
-	return regmap_update_bits(rst->regmap, map->reg, mask, 0);
+	return qcom_update_reset(rst, map, mask, 0);
 }
 
 const struct reset_control_ops qcom_reset_ops = {
