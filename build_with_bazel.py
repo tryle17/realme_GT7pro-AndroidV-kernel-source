fbe2abcfa03088f636444b8fbfe42b6972bb7dee
Satya Durga Srinivasu Prabhala <quic_satyap@quicinc.com>
2024-02-08 17:40:47 -0800

    ANDROID: build: pass --incompatible_sandbox_hermetic_tmp=false
    As we now set default cache directory with in the workspace, with the
    updated bazel we see some symlinks are still referring to /tmp and
    causing build issues.
    
    To avoid build issue, set incompatible_sandbox_hermetic_tmp to false
    and pass it to bazel, so that, bazel creates and later cleans up a
    dedicated, initially empty temporary directory for each sandboxed action.
    
    Change-Id: Ic55936c80461bdfab603105cb21ff9143acbc8fa
    Signed-off-by: Satya Durga Srinivasu Prabhala <quic_satyap@quicinc.com>

--- build_with_bazel.py
+++ build_with_bazel.py
@@ -295,6 +295,7 @@ class BazelBuilder:
         self.user_opts.extend([
             "--user_kmi_symbol_lists=//msm-kernel:android/abi_gki_aarch64_qcom",
             "--ignore_missing_projects",
+            "--incompatible_sandbox_hermetic_tmp=false",
         ])
 
         if self.dry_run:
