89d8f20ca6dcb43bd7494d6430c3f15647ac6f4f
Mukesh Ojha <quic_mojha@quicinc.com>
2024-04-01 22:34:30 +0530

    nvmem: qfprom: Add keepout region for Sun
    Add keepout region for Sun SoC.
    
    Change-Id: I10d5c3c6d8cb83449ca3c12f4412ec5c9c94c2a8
    Signed-off-by: Mukesh Ojha <quic_mojha@quicinc.com>

--- drivers/nvmem/qfprom.c
+++ drivers/nvmem/qfprom.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (C) 2015 Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
- * Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021,2024 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/clk.h>
@@ -124,6 +124,34 @@ static const struct nvmem_keepout sc7280_qfprom_keepout[] = {
 static const struct qfprom_soc_compatible_data sc7280_qfprom = {
 	.keepout = sc7280_qfprom_keepout,
 	.nkeepout = ARRAY_SIZE(sc7280_qfprom_keepout)
+};
+
+static const struct nvmem_keepout sun_qfprom_keepout[] = {
+	{.start = 0x20, .end = 0x24},
+	{.start = 0x28, .end = 0x30},
+	{.start = 0x34, .end = 0x40},
+	{.start = 0x58, .end = 0x60},
+	{.start = 0x68, .end = 0x70},
+	{.start = 0x78, .end = 0x80},
+	{.start = 0x90, .end = 0x100},
+	{.start = 0x150, .end = 0x200},
+	{.start = 0x238, .end = 0x300},
+	{.start = 0x330, .end = 0x400},
+	{.start = 0x4e8, .end = 0x500},
+	{.start = 0x550, .end = 0x600},
+	{.start = 0x608, .end = 0x610},
+	{.start = 0x618, .end = 0x630},
+	{.start = 0x638, .end = 0x700},
+	{.start = 0x738, .end = 0x73c},
+	{.start = 0x74c, .end = 0x770},
+	{.start = 0x900, .end = 0x948},
+	{.start = 0xbf0, .end = 0xcf0},
+	{.start = 0xd00, .end = 0x1000},
+};
+
+static const struct qfprom_soc_compatible_data sun_qfprom = {
+	 .keepout = sun_qfprom_keepout,
+	 .nkeepout = ARRAY_SIZE(sun_qfprom_keepout)
 };
 
 /**
@@ -460,6 +488,7 @@ static const struct of_device_id qfprom_of_match[] = {
 	{ .compatible = "qcom,qfprom",},
 	{ .compatible = "qcom,sc7180-qfprom", .data = &sc7180_qfprom},
 	{ .compatible = "qcom,sc7280-qfprom", .data = &sc7280_qfprom},
+	{ .compatible = "qcom,sun-qfprom",    .data = &sun_qfprom},
 	{/* sentinel */},
 };
 MODULE_DEVICE_TABLE(of, qfprom_of_match);
