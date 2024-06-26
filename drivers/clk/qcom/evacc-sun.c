f60acbc1e809626f5822123dd486de8546ba369b
Kalpak Kawadkar <quic_kkawadka@quicinc.com>
2024-06-26 22:10:13 +0530

    clk: qcom: Use off instead of standby for taycan pll's
    Rare lock failures are seen for taycan plls on MXC rail.
    Try turning the PLL completely off instead of using standby.
    This will reset the PLL and hopefully prevent the lock failures.
    
    Signed-off-by: Kalpak Kawadkar <quic_kkawadka@quicinc.com>
    Signed-off-by: Bruce Levy <quic_blevy@quicinc.com>
    Change-Id: I0c872d8dd2db721e432538b95363acd406204af5

--- drivers/clk/qcom/evacc-sun.c
+++ drivers/clk/qcom/evacc-sun.c
@@ -60,6 +60,7 @@ static struct clk_alpha_pll eva_cc_pll0 = {
 	.vco_table = taycan_elu_vco,
 	.num_vco = ARRAY_SIZE(taycan_elu_vco),
 	.regs = clk_alpha_pll_regs[CLK_ALPHA_PLL_TYPE_TAYCAN_ELU],
+	.flags = DISABLE_TO_OFF,
 	.clkr = {
 		.hw.init = &(const struct clk_init_data) {
 			.name = "eva_cc_pll0",
