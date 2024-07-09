1310a422b2d2218b53ee8c2fe5149b6982be23cc
Bruce Levy <quic_blevy@quicinc.com>
2024-07-08 21:52:42 -0700

    Revert "remoteproc: pas: check if there is a crash in rproc_set_state"
    This reverts commit 1e9a05c95b1402f388352cad46b6ba31b763c13e.
    
    Signed-off-by: Bruce Levy <quic_blevy@quicinc.com>
    Change-Id: I243bc5d03a00364f3587d801f2af383bf73defeb

--- drivers/remoteproc/qcom_q6v5_pas.c
+++ drivers/remoteproc/qcom_q6v5_pas.c
@@ -856,18 +856,14 @@ int rproc_set_state(struct rproc *rproc, bool state)
 		}
 
 		ret = rproc_config_check(adsp, SOCCP_D0);
-		dev_err(adsp->dev, "%s requested D3->D0: soccp returned tcsr: tcsr val=%d\n",
-			current->comm, readl(adsp->config_addr));
 		if (ret) {
-			dev_err(adsp->dev, "%s requested D3->D0: soccp failed to update tcsr: tcsr val=%d\n",
-				current->comm, readl(adsp->config_addr));
+			dev_err(adsp->dev, "failed to change from D3 to D0\n");
 			goto soccp_out;
 		}
 
 		ret = wait_for_completion_timeout(&adsp->q6v5.running_ack, msecs_to_jiffies(5));
 		if (!ret) {
-			dev_err(adsp->dev, "%s requested D3->D0: failed to get wake ack\n",
-				current->comm);
+			dev_err(adsp->dev, "failed to get ack for state change from D3 to D0\n");
 			ret = -ETIMEDOUT;
 			goto soccp_out;
 		} else
@@ -889,11 +885,8 @@ int rproc_set_state(struct rproc *rproc, bool state)
 			}
 
 			ret = rproc_config_check(adsp, SOCCP_D3);
-			dev_err(adsp->dev, "%s requested D0->D3: soccp returned tcsr: tcsr val=%d\n",
-				current->comm, readl(adsp->config_addr));
 			if (ret) {
-				dev_err(adsp->dev, "%s requested D0->D3 failed: TCSR value:%d\n",
-					current->comm, readl(adsp->config_addr));
+				dev_err(adsp->dev, "failed to change from D0 to D3\n");
 				goto soccp_out;
 			}
 			disable_regulators(adsp);
@@ -903,15 +896,9 @@ int rproc_set_state(struct rproc *rproc, bool state)
 	}
 
 soccp_out:
-	if (ret && (adsp->rproc->state != RPROC_RUNNING)) {
-		dev_err(adsp->dev, "SOCCP has crashed while processing a D"
-			"transition req by %s\n", current->comm);
-		ret = -EBUSY;
-	}
-
 	mutex_unlock(&adsp->adsp_lock);
 
-	return ret;
+	return ret ? -ETIMEDOUT : 0;
 }
 EXPORT_SYMBOL_GPL(rproc_set_state);
 
