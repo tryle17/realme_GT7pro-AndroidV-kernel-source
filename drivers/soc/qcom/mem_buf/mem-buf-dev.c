3676a058e5ffc45a24cfdd754bacde0095f71a65
Patrick Daly <quic_pdaly@quicinc.com>
2024-07-24 16:39:11 -0700

    mem-buf: Fix retry mechanism for reclaim operation
    Although gunyah returns 11 for the case we want to retry for, this error
    code is overridden with -EINVAL by the time it is passed back to the
    mem-buf driver. Retry on all failures instead.
    
    Also fix an issue where the incorrect variable is used when deciding
    whether or not to display a message. num_retries represents the
    maximum possible number of retries. i represents the number of retries
    which were required.
    
    Change-Id: Ia962e87235bd2355739560446f33a5c2559d0558
    Fixes: 7f5dddd051e4 ("mem-buf: Retry reclaim several times on failure")
    Signed-off-by: Patrick Daly <quic_pdaly@quicinc.com>

--- drivers/soc/qcom/mem_buf/mem-buf-dev.c
+++ drivers/soc/qcom/mem_buf/mem-buf-dev.c
@@ -33,11 +33,7 @@ EXPORT_SYMBOL_GPL(dmabuf_mem_pool);
 
 #define POOL_MIN_ALLOC_ORDER SUBSECTION_SHIFT
 
-/* Gunyah RM error codes */
-#define GH_RM_ERR_MEM_INUSE	0x1B
-#define GH_RM_ERR_MEM_RELEASED	0xC
-
-#define RECLAIM_RETRY_DELAY_US	100
+#define RECLAIM_RETRY_DELAY_MS	100
 
 int mem_buf_hyp_assign_table(struct sg_table *sgt, u32 *src_vmid, int source_nelems,
 			     int *dest_vmids, int *dest_perms, int dest_nelems)
@@ -108,23 +104,25 @@ int mem_buf_unassign_mem(struct sg_table *sgt, int *src_vmids,
 		 */
 		for (i = 0; i < num_retries; i++) {
 			ret = mem_buf_unassign_mem_gunyah(memparcel_hdl);
-
-			/* retry only for MEM_INUSE cases */
-			if (!ret || ret != GH_RM_ERR_MEM_INUSE)
+			/*
+			 * Although gunyah returns 11 for the case we want to retry for, this error
+			 * code is overridden with -EINVAL by the time it is passed back to us.
+			 * Retry on all failures instead.
+			 */
+			if (!ret)
 				break;
 
-			msleep(RECLAIM_RETRY_DELAY_US);
+			msleep(RECLAIM_RETRY_DELAY_MS);
 		}
 		if (ret) {
-			if (num_retries)
-				pr_err_ratelimited("mem_buf_unassign_mem_gunyah: handle %d failed after %d retries\n",
-				memparcel_hdl, i);
+			pr_err_ratelimited("mem_buf_unassign_mem_gunyah: handle %d failed after %d retries\n",
+					    memparcel_hdl, i);
 			return ret;
 		}
 
-		if (num_retries)
+		if (i)
 			pr_info_ratelimited("mem_buf_unassign_mem_gunyah: handle %d succeeeded after %d retries\n",
-				 memparcel_hdl, i);
+					    memparcel_hdl, i);
 	}
 
 	ret = mem_buf_hyp_assign_table(sgt, src_vmids, nr_acl_entries,
