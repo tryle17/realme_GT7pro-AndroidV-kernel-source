9f32c406fec723490f21c3d6cf49d5f7f4ba3aad
Linux Image Build Automation <quic_ibautomat@quicinc.com>
2024-08-15 20:11:22 -0700

    Revert "mpam: msc: SLC mapam SYSFS driver support"
    This reverts commit 7742e20bdde3efce54b4abbade875c7eecc5997b.
    
    Change-Id: I43becd46443694284b7fc2bb3072643b2f633454
    Signed-off-by: Linux Image Build Automation <quic_ibautomat@quicinc.com>

--- drivers/soc/qcom/mpam/mpam_msc.c
+++ drivers/soc/qcom/mpam/mpam_msc.c
@@ -183,171 +183,6 @@ void detach_dev(struct device *dev, struct qcom_mpam_msc *qcom_msc, uint32_t msc
 }
 EXPORT_SYMBOL_GPL(detach_dev);
 
-static ssize_t slc_dev_capability_show(struct device *dev, struct device_attribute *attr,
-		char *buf)
-{
-	ssize_t count = 0;
-	int client_idx = 0, partid_idx = 0, ret;
-	struct qcom_slc_capability *slc_capability;
-	struct  slc_client_capability *slc_client_cap;
-	struct slc_partid_capability *slc_partid_cap;
-	struct qcom_mpam_msc *qcom_mpam_msc;
-	struct msc_query query;
-	struct qcom_slc_gear_val gear_config;
-
-	qcom_mpam_msc = qcom_msc_lookup(2);
-	if (qcom_mpam_msc == NULL)
-		return count;
-
-	slc_capability =  (struct qcom_slc_capability *)qcom_mpam_msc->msc_capability;
-
-	count += scnprintf(buf + count, PAGE_SIZE - count,
-			"MSC_ID:<%02d>, MSC_NAME:<%13s>, qcom_msc_id: <%d>",
-		qcom_mpam_msc->msc_id, qcom_mpam_msc->msc_name,
-		qcom_mpam_msc->qcom_msc_id.idx);
-
-	query.qcom_msc_id.qcom_msc_type = qcom_mpam_msc->qcom_msc_id.qcom_msc_type;
-	query.qcom_msc_id.qcom_msc_class = qcom_mpam_msc->qcom_msc_id.qcom_msc_class;
-	query.qcom_msc_id.idx = qcom_mpam_msc->qcom_msc_id.idx;
-
-	count += scnprintf(buf + count, PAGE_SIZE - count, "\n");
-	for (client_idx = 0; client_idx < slc_capability->num_clients; client_idx++) {
-		slc_client_cap = &(slc_capability->slc_client_cap[client_idx]);
-		count += scnprintf(buf + count, PAGE_SIZE - count,
-				"\t  Client Name:<%13s>,   Client ID: <%d>, Number of Part ID's: <%d>,\tSupport: %s\n",
-				slc_client_cap->client_name,
-				slc_client_cap->client_info.client_id,
-				slc_client_cap->client_info.num_part_id,
-				slc_client_cap->enabled ? "ENABLED" : "DISABLED");
-
-		if (slc_client_cap->enabled == false)
-			continue;
-
-		query.client_id = slc_client_cap->client_info.client_id;
-		for (partid_idx = 0; partid_idx < slc_client_cap->client_info.num_part_id; partid_idx++) {
-			slc_partid_cap = &(slc_client_cap->slc_partid_cap[partid_idx]);
-			count += scnprintf(buf + count, PAGE_SIZE - count,
-					"\t\t\t  Part id:<%d>,   num gears: <%d>,",
-					slc_partid_cap->part_id,
-					slc_partid_cap->num_gears);
-
-			query.part_id = partid_idx;
-			ret = msc_system_get_partition(qcom_mpam_msc->msc_id, &query, &gear_config);
-			if (ret || (gear_config.gear_val >= GEAR_MAX)) {
-				gear_config.gear_val = 0;
-				count += scnprintf(buf + count, PAGE_SIZE - count,
-						" Gear configured Improperly!\n");
-				continue;
-			}
-
-			count += scnprintf(buf + count, PAGE_SIZE - count, " Gear val Configured: %s\n",
-					gear_index[gear_config.gear_val]);
-		}
-	}
-
-	return count;
-}
-
-static ssize_t slc_part_config_store(struct device *dev, struct device_attribute *attr,
-		const char *buf, size_t count)
-{
-	char *token, *delim = DELIM_CHAR;
-	struct msc_query query;
-	struct qcom_slc_gear_val gear_config;
-	unsigned long val;
-	struct qcom_mpam_msc *qcom_mpam_msc;
-
-	qcom_mpam_msc = qcom_msc_lookup(2);
-	if (qcom_mpam_msc == NULL)
-		return count;
-
-	query.qcom_msc_id.qcom_msc_type = qcom_mpam_msc->qcom_msc_id.qcom_msc_type;
-	query.qcom_msc_id.qcom_msc_class = qcom_mpam_msc->qcom_msc_id.qcom_msc_class;
-	query.qcom_msc_id.idx = qcom_mpam_msc->qcom_msc_id.idx;
-
-	token = strsep((char **)&buf, delim);
-	if (token == NULL)
-		return count;
-
-	if (kstrtoul(token, 0, &val))
-		return count;
-
-	query.client_id = (uint16_t) val;
-
-	token = strsep((char **)&buf, delim);
-	if (token == NULL)
-		return count;
-
-	if (kstrtoul(token, 0, &val))
-		return count;
-
-	query.part_id = (uint16_t) val;
-
-	token = strsep((char **)&buf, delim);
-	if (token == NULL)
-		return count;
-
-	if (kstrtoul(token, 0, &val))
-		return count;
-
-	gear_config.gear_val = val;
-	msc_system_set_partition(qcom_mpam_msc->msc_id, &query, &gear_config);
-	return count;
-}
-
-static ssize_t slc_dev_reset_store(struct device *dev, struct device_attribute *attr,
-		const char *buf, size_t count)
-{
-	int j = 0, k = 0, ret;
-	struct qcom_slc_capability *slc_capability;
-	struct  slc_client_capability *slc_client_cap;
-	struct slc_partid_capability *slc_partid_cap;
-	struct qcom_mpam_msc *qcom_mpam_msc;
-	struct msc_query query;
-	struct qcom_slc_gear_val gear_config;
-
-	qcom_mpam_msc = qcom_msc_lookup(2);
-	if (qcom_mpam_msc == NULL)
-		return count;
-
-	slc_capability =  (struct qcom_slc_capability *)qcom_mpam_msc->msc_capability;
-
-	query.qcom_msc_id.qcom_msc_type = qcom_mpam_msc->qcom_msc_id.qcom_msc_type;
-	query.qcom_msc_id.qcom_msc_class = qcom_mpam_msc->qcom_msc_id.qcom_msc_class;
-	query.qcom_msc_id.idx = qcom_mpam_msc->qcom_msc_id.idx;
-
-	for (j = 0; j < slc_capability->num_clients; j++) {
-		slc_client_cap = &(slc_capability->slc_client_cap[j]);
-
-		if (slc_client_cap->enabled == false)
-			continue;
-
-		query.client_id = slc_client_cap->client_info.client_id;
-		for (k = 0; k < slc_client_cap->client_info.num_part_id; k++) {
-			slc_partid_cap = &(slc_client_cap->slc_partid_cap[k]);
-
-			query.part_id = k;
-			gear_config.gear_val = 0;
-			ret = msc_system_reset_partition(qcom_mpam_msc->msc_id, &query, &gear_config);
-			if (ret)
-				continue;
-		}
-	}
-
-	return count;
-}
-
-static DEVICE_ATTR_RO(slc_dev_capability);
-static DEVICE_ATTR_WO(slc_part_config);
-static DEVICE_ATTR_WO(slc_dev_reset);
-
-static struct attribute *mpam_slc_sysfs_attrs[] = {
-	&dev_attr_slc_dev_capability.attr,
-	&dev_attr_slc_part_config.attr,
-	&dev_attr_slc_dev_reset.attr,
-	NULL,
-};
-
 static struct attribute_group mpam_slc_sysfs_group = {
 	.attrs	= mpam_slc_sysfs_attrs,
 };
@@ -355,12 +190,6 @@ static struct attribute_group mpam_slc_sysfs_group = {
 static int mpam_msc_probe(struct platform_device *pdev)
 {
 	int ret;
-
-	ret = sysfs_create_group(&pdev->dev.kobj, &mpam_slc_sysfs_group);
-	if (ret) {
-		pr_err("Unable to create sysfs group\n");
-		return ret;
-	}
 
 	if (of_platform_populate(pdev->dev.of_node, NULL, NULL, &pdev->dev) < 0)
 		dev_err(&pdev->dev, "Mpam driver probe failed.!\n");
@@ -370,7 +199,6 @@ static int mpam_msc_probe(struct platform_device *pdev)
 
 int mpam_msc_remove(struct platform_device *pdev)
 {
-	sysfs_remove_group(&pdev->dev.kobj, &mpam_slc_sysfs_group);
 	return 0;
 }
 static const struct of_device_id mpam_msc_table[] = {
