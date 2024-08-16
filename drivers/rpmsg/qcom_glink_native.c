51743427cff563dee7d9ff00d3daaa6d21b600d4
Linux Image Build Automation <quic_ibautomat@quicinc.com>
2024-08-15 20:05:26 -0700

    Revert "rpmsg: glink: Retry in case of reading unhandled cmd"
    This reverts commit d482c1e87993886ecbc3f2c1dab27ca316b26499.
    
    Change-Id: I68b327d63d323486613c6c5c422c78d2a0d3a8a4
    Signed-off-by: Linux Image Build Automation <quic_ibautomat@quicinc.com>

--- drivers/rpmsg/qcom_glink_native.c
+++ drivers/rpmsg/qcom_glink_native.c
@@ -1544,7 +1544,6 @@ void qcom_glink_native_rx(struct qcom_glink *glink)
 	unsigned int param2;
 	unsigned int avail;
 	unsigned int cmd;
-	int retry = 0;
 	int ret = 0;
 
 	if (should_wake) {
@@ -1607,14 +1606,9 @@ void qcom_glink_native_rx(struct qcom_glink *glink)
 			qcom_glink_handle_signals(glink, param1, param2);
 			break;
 		default:
-			dev_err(glink->dev, "unhandled rx cmd: 0x%x\n", cmd);
-			retry++;
-			if (retry < 5)
-				continue;
-			else {
-				ret = -EINVAL;
-				break;
-			}
+			dev_err(glink->dev, "unhandled rx cmd: %d\n", cmd);
+			ret = -EINVAL;
+			break;
 		}
 
 		if (ret)
